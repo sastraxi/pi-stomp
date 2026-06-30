3a94ae21b300eab08d4c211649c452f91e23ceb0
Cam Gorrie <sastraxi@gmail.com>
2026-06-29 23:23:10 -0400

    Finish the plan

--- plugins/capseq10/__init__.py
+++ plugins/capseq10/__init__.py
@@ -3,8 +3,10 @@
 from plugins.customization import PluginCustomization, register
 from plugins.capseq10.panel import CapsEq10Panel
 
+CAPSEQ10_URI = "http://moddevices.com/plugins/caps/Eq10"
+
 register(
-    "http://moddevices.com/plugins/caps/Eq10",
+    CAPSEQ10_URI,
     customization=PluginCustomization(
         panel_cls=CapsEq10Panel,
         display_name="caps-Eq10",
